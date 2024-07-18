ccb61c2c8a790ec8b3d6e3d31cb82b576ca1d595
Poojadhav2611 <poojajadhav291199@gmail.com>
2024-07-18 14:40:20 -0700

    added

--- first.js
+++ first.js
@@ -1,1 +1,57 @@
-console.log("Pooja Jadhav")
+class Person
+{
+    constructor(firstName,lastName)
+    {
+this.firstName=firstName
+this.lastName=lastName
+    }
+    displayName()
+    {
+        console.log(this.firstName+this.lastName)
+    }
+}
+let PoojaJ=new Person("Pooja","Jadhav")
+PoojaJ.displayName()
+
+//hirarchical
+class Mother {
+    constructor(fn,ln){
+        this.firstName=fn
+        this.lastName=ln
+    }
+    displayName(){
+        console.log(this.firstName + this.lastName )
+    }
+}
+
+class Son extends Mother{
+    constructor(fn,ln,sfn){
+        super(fn,ln)
+        this.sFirstName=sfn
+    }
+    displaySname(){
+        console.log(this.lastName  + this.sFirstName)
+
+
+    }
+    
+}
+
+class Daughter extends Mother{
+    constructor(fn,ln,dfn){
+        super(fn,ln)
+        this.dFirstName=dfn
+    }
+    displayDname(){
+        console.log(this.lastName + this.dFirstName)
+    }
+}
+let Pooja=new Daughter("Sarika","Jadhav","Pooja")
+let Pari=new Son("Sarika","Jadhav","Pari")
+Pooja.displayDname()
+Pooja.displayName()
+Pari.displaySname()
+
+//multilevel 
+
+console.log("Testing")
